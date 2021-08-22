d599a943cd6b593da72f796218be74e00b22a986
suryaprakash <singhyash2207@gmail.comm>
2021-08-22 11:03:33 +0530

    Initial commit: drive car

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int age=16;
+        if(age>18){
+            System.out.println("Yes you can drive");
+        }
+        else {
+            System.out.println("No you can not drive yet");
+        }
+    }
+}
